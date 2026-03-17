963cff53b1c68eea85a0d535c60f653677c370c4
joaovieiraalmeida14-cpu <joao.vieira.almeida14@escola.pr.gov.br>
2026-03-17 07:41:00 -0300

    Add files via upload

---
+++ SQLite (2).sql
@@ -0,0 +1,7 @@
+CREATE TABLE produtos (
+id integer primary key AUTOINCREMENT,
+  codigo integer,
+  nome text,
+  preco real,
+  quantidade integer
+);
---
+++ SQLite.1.sql
@@ -0,0 +1,2 @@
+INSERT INTO produtos (codigo, nome, preco, quantidade)
+VALUES (020, 'webcam', 67, 76);
